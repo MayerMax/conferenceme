00b604068e24d9b7f55994d2ed70d72488192e8a
MaxMayer <endurancemayer@yandex.ru>
2018-03-30 01:02:36 +0500

    абстрактный replier, добавленл enum для состояний пользователя относительно бота

---
+++ bot/service/repliers/abstract_replier.py
@@ -0,0 +1,30 @@
+import abc
+from typing import List
+
+from bot.query import QueryResult
+from bot.statuses import UserState
+
+
+class AbstractReplier(metaclass=abc.ABCMeta):
+    def __init__(self, root_state_names: List[str], default_state_names: List[str] = None):
+        """
+        конструктор абстрактного replier-a
+        :param root_state_names: именя вершин со статусом ROOT
+        :param default_state_names: имена вершин, которые доступные всегда, независимо от состояний
+        """
+        self.root_state_names = root_state_names
+        self.default_state_names = default_state_names
+        self.user_state = UserState.GUEST
+
+    def set_user_state(self, new_state: UserState):
+        self.user_state = UserState
+
+    @abc.abstractmethod
+    def create_reply(self, query_result: QueryResult) -> object:
+        """
+        абстрактная функция, реализующая поведение replier-а.
+        Каждый мессенджер должен реализовывать ее, чтобы подготовить ответ, который может быть отправлен ботом
+        :param query_result: результат, который подготовил анализатор, и который должен быть представлен replier-ом
+        :return: объект произвольного типа, который может быть отправлен ботом пользователю
+        """
+        pass
