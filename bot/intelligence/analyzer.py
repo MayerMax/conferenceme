0a868e011fc0baa779170661594acd8ef8dea62d
MaxMayer <endurancemayer@yandex.ru>
2018-03-31 17:15:06 +0500

    добавлен класс контекста пользователя

--- bot/intelligence/analyzer.py
+++ bot/intelligence/analyzer.py
@@ -1,4 +1,5 @@
 from bot.service.conference_plain_object import ConferencePlainObject
+from bot.service.history import Context
 from bot.terminated_core.graph.create import create_graph
 from bot.terminated_core.graph.state_graph import StateGraph
 
@@ -9,5 +10,8 @@ class Analyzer:
         self.__graph = create_graph()
         self.user_context = {}
 
-    def analyze(self, user_raw_query: str, user_data: dict, search_source: ConferencePlainObject):
-        pass
+    def analyze(self, user_raw_query: str, user_data, search_source: ConferencePlainObject):
+        if user_data.username not in self.user_context:
+            self.user_context[user_data.username] = Context(user_data.username)
+        user_context = self.user_context.get(user_data.username)
+        
