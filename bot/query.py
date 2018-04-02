bfd370feaa69aa2106a33fceccec76aff18bb419
MaxMayer <endurancemayer@yandex.ru>
2018-04-02 14:04:37 +0500

    добавлен класс пользовательского запроса (QueryRequest)

--- bot/query.py
+++ bot/query.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from bot.service.conference_plain_object import ConferencePlainObject
+from bot.service.users.user import User
 from bot.statuses import StatusTypes
 
 
@@ -22,3 +24,20 @@ class QueryResult:
         self.answer = answer
         self.attachments = attachments
         self.extra_args = extra_args
+
+
+class QueryRequest:
+    """
+    Класс, который формирует запрос, принимаемый интерфейсами для разных мессенджеров, чтобы отдать анализатору
+    """
+
+    def __init__(self, who_asked: User, question: str, where_to_search: ConferencePlainObject):
+        """
+        конструктор
+        :param who_asked: пользователь мессенджера
+        :param question: сырой запрос пользователя
+        :param where_to_search: объект конференции, в котором осуществляется поиск
+        """
+        self.who_asked = who_asked
+        self.question = question
+        self.where_to_search = where_to_search
